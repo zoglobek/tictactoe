e50597b15bcb9f6a8907efac7c1492eb64ccc556
gal <newmangal@msn.com>
2025-12-21 16:47:18 +0200

    learnt to change to other fonts

--- board.py
+++ board.py
@@ -15,11 +15,11 @@ window = tk.Tk()
 window.title("TICK TACK TOE")
 window.resizable(False,False)
 frame = tk.Frame(window)
-label = tk.Label(frame, text=("Current player is", CURRENT_PLAYER), background=gray, foreground=white, font=("Consolas", 20))
+label = tk.Label(frame, text=("Current player is", CURRENT_PLAYER), background=gray, foreground=white, font=("Arial", 20))
 label.grid(row=0, column=0)
 for row in range(3):
     for column in range(3):
-        BOARD[row][column] = tk.Button(frame, text="", font=("Consolas", 50, "bold"), background=gray)
+        BOARD[row][column] = tk.Button(frame, text="", font=("Arial", 50, "bold"), background=gray)
 frame.pack(fill="both",expand=True)
 window.mainloop()
 
