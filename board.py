1303972bffeae45e452f1a0f8ddd6a022aaee562
gal <newmangal@msn.com>
2025-12-21 05:01:42 +0200

    started gui build and player input also added init file for repo

---
+++ board.py
@@ -0,0 +1,17 @@
+import tkinter
+BOARD = [[0,0,0,],
+         [0,0,0],
+         [0,0,0]]
+def set_tile(row, column):
+    ...
+
+
+#gui setup
+window = tkinter.Tk()
+window.title("TICK TACK TOE")
+window.resizable(False,False)
+
+frame = tkinter.Frame(window)
+
+if __name__ == "__main__":
+    ...
---
+++ player_input.py
@@ -0,0 +1,5 @@
+PLAYER_1 = "X"
+PLAYER_2 = "O"
+CURRENT_PLAYER = PLAYER_1
+if __name__ == "__main__":
+    ...
