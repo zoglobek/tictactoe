16477c3f230d67df6ddcada863b02cd92fc7e1f0
gal <newmangal@msn.com>
2025-12-21 12:41:11 +0200

    working on gui  stuck on understanding frames in gui

--- board.py
+++ board.py
@@ -1,4 +1,6 @@
-import tkinter
+import tkinter as tk
+from tictactoe.player_input import CURRENT_PLAYER
+
 BOARD = [[0,0,0,],
          [0,0,0],
          [0,0,0]]
@@ -7,11 +9,19 @@ def set_tile(row, column):
 
 
 #gui setup
-window = tkinter.Tk()
+gray = "#808080"
+white = "#FFFFFF"
+window = tk.Tk()
 window.title("TICK TACK TOE")
 window.resizable(False,False)
-
-frame = tkinter.Frame(window)
+frame = tk.Frame(window)
+label = tk.Label(frame, text=("Current player is", CURRENT_PLAYER), background=gray, foreground=white, font=("Consolas", 20))
+label.grid(row=0, column=0)
+for row in range(3):
+    for column in range(3):
+        BOARD[row][column] = tk.Button(frame, text="", font=("Consolas", 50, "bold"), background=gray)
+frame.pack(fill="both",expand=True)
+window.mainloop()
 
 if __name__ == "__main__":
     ...
