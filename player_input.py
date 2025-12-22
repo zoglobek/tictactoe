6a3edc33eafb0dce10500a9975e20882ea486741
gal <newmangal@msn.com>
2025-12-22 13:08:01 +0200

    finished primary gui look stuck on changable color scheme and input made logic function skeleton for later build

--- player_input.py
+++ player_input.py
@@ -1,5 +1,28 @@
-PLAYER_1 = "X"
-PLAYER_2 = "O"
-CURRENT_PLAYER = PLAYER_1
+#COLOR SCHEME
+black = "#000000"
+lime = "#BBF90F"
+gray = "#808080"
+white = "#FFFFFF"
+yellow = "#FFFF00"
+blue = "#000080"
+red = "#FF0000"
+
+X = "X"
+O = "O"
+CURRENT_PLAYER = X
+
+def curr_player_color():
+    if CURRENT_PLAYER == X:
+        player_color = red
+    elif CURRENT_PLAYER == O:
+        player_color = blue
+    return player_color
+
+
+def switch_player():
+    ...
+def is_tile_set():
+    ...
+
 if __name__ == "__main__":
     ...
