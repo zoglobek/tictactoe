a5fa9ce8cc6e9125bb2283f868159ca41b450eb7
gal <newmangal@msn.com>
2025-12-22 18:34:40 +0200

    started main added some fixes to board started defining player input functions

---
+++ main.py
@@ -0,0 +1,8 @@
+from board import board
+
+
+
+
+
+if __name__ == "__main__":
+    board()
--- player_input.py
+++ player_input.py
@@ -9,7 +9,12 @@ red = "#FF0000"
 
 X = "X"
 O = "O"
+global CURRENT_PLAYER
 CURRENT_PLAYER = X
+
+def set_tile(row, column):
+    ...
+
 
 def curr_player_color():
     if CURRENT_PLAYER == X:
@@ -19,8 +24,15 @@ def curr_player_color():
     return player_color
 
 
-def switch_player():
-    ...
+def switch_player(CURRENT_PLAYER):
+    if CURRENT_PLAYER == X:
+        CURRENT_PLAYER = O
+        return CURRENT_PLAYER
+    elif CURRENT_PLAYER == O:
+         CURRENT_PLAYER = X
+         return CURRENT_PLAYER
+
+
 def is_tile_set():
     ...
 
