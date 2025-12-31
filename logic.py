a0557db7f37e8aee3f0aa7aa57031fa4485e29e2
gal <newmangal@msn.com>
2025-12-31 14:21:47 +0200

    built most of the logic functions for checking 3 of a kind in each directions complete and tested still figuring check winner out_of_tiles tie and is_gameover

--- logic.py
+++ logic.py
@@ -1,6 +1,8 @@
 from player_input import player_input, X, O
 from board import places, row_1, row_2, row_3, column_2, column_3, column_1, diagonal_1, diagonal_2, all_tiles
 
+three_x = [X, X, X]
+three_o = [O, O, O]
 
 
 def is_tile_set(CURRENT_PLAYER):
@@ -13,63 +15,51 @@ def is_tile_set(CURRENT_PLAYER):
             places.update({tile: CURRENT_PLAYER})
             break
 
-def three_in_row():
-    for place in row_1:
-        if place == X or place == O:
+def three_in_row(row_1, row_2, row_3):
+    all_rows = row_1, row_2, row_3
+    global three_x
+    global three_o
+    for row in all_rows:
+        if row == three_x:
             return X
-        else:
+        elif row == three_o:
             return O
-    for place in row_2:
-        if place == X or place == O:
+
+def three_in_coulmn(column_1, column_2, column_3):
+    all_columns = column_1, column_2, column_3
+    global three_x
+    global three_o
+    for column in all_columns:
+        if column == three_x:
             return X
-        else:
+        elif column == three_o:
             return O
-    for place in row_3:
-        if place == X or place == O:
-            return X
-        else:
-            return O
-    else:
-        return False
 
 
-def three_in_coulmn():
-    for place in column_1:
-        if place == X or place == O:
+def three_diagonal(diagonal_1, diagonal_2):
+    all_diagonals = diagonal_1, diagonal_2
+    global three_x
+    global three_o
+    for diagonal in all_diagonals:
+        if diagonal == three_x:
             return X
-        else:
+        elif diagonal == three_o:
             return O
-    for place in column_2:
-        if place == X or place == O:
-            return X
-        else:
-            return O
-    for place in column_3:
-        if place == X or place == O:
-            return X
-        else:
-            return O
-    else:
-        return False
 
-
-def three_diagonal():
-    for place in diagonal_1:
-        if place == X or place == O:
-            return X
-        else:
-            return O
-    for place in diagonal_2:
-        if place == X or place == O:
-            return X
-        else:
-            return O
-    else:
-        return False
+def out_of_tiles():
+    if all_tiles:
+        print("TIE")
+        return True
 
 
 def check_winner():
-    ...
+    if three_diagonal():
+        print(three_diagonal())
+    if three_in_coulmn():
+        print(three_in_coulmn())
+    if three_in_row():
+        print(three_in_row())
+
 
 def is_gameover():
     if all_tiles:
@@ -79,4 +69,5 @@ def is_gameover():
 
 if __name__ == "__main__":
     ...
-    # is_tile_set(CURRENT_PLAYER)
+
+    print(three_in_row(row_1, row_2, row_3))
