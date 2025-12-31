a056f81a35d99d97cfe482674c62d8cc7d63e3b2
gal <newmangal@msn.com>
2025-12-31 11:57:31 +0200

    fixed the issue with the infinite invalid tile loop  in logic  started writing the winning conditions fixed typos in board for the diagonal lines definitions from capital to lower letter

--- board.py
+++ board.py
@@ -2,6 +2,10 @@ places = {1: '1', 2: '2', 3: '3',
           4: '4', 5: '5', 6: '6',
           7: '7', 8: '8', 9: '9'}
 
+
+all_tiles = [places[1],places[2], places[3],
+             places[4],places[5], places[6],
+             places[7], places[8], places[9]]
 #Rows
 row_1 = [places[1],places[2], places[3]]
 row_2 = [places[4],places[5], places[6]]
@@ -13,8 +17,8 @@ column_2 = [places[2],places[5], places[8]]
 column_3 = [places[3],places[6], places[9]]
 
 #Diagonals
-Diagonal_1 = [places[1],places[5], places[9]]
-Diagonal_2 = [places[3],places[5], places[7]]
+diagonal_1 = [places[1],places[5], places[9]]
+diagonal_2 = [places[3],places[5], places[7]]
 
 
 def board_draw():
@@ -28,7 +32,8 @@ def board_draw():
 
 if __name__ == "__main__":
     board_draw()
-    print(row_1)
-    print(Diagonal_1)
+    print(row_1[1])
+    print(diagonal_1)
     print(column_1)
     print(row_3)
+    print(all_tiles)
--- logic.py
+++ logic.py
@@ -1,26 +1,79 @@
 from player_input import player_input, X, O
-from board import places
+from board import places, row_1, row_2, row_3, column_2, column_3, column_1, diagonal_1, diagonal_2, all_tiles
+
 
 
 def is_tile_set(CURRENT_PLAYER):
-    tile = player_input(CURRENT_PLAYER)
-    while places[tile] == X or places[tile] == O:
-        print("invalid tile")
+    while True:
+        tile = player_input(CURRENT_PLAYER)
+        if places[tile] == X or places[tile] == O:
+            print("invalid tile")
+        else:
+            print("valid")
+            places.update({tile: CURRENT_PLAYER})
+            break
+
+def three_in_row():
+    for place in row_1:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    for place in row_2:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    for place in row_3:
+        if place == X or place == O:
+            return X
+        else:
+            return O
     else:
-        print("valid")
-        places.update({tile: CURRENT_PLAYER})
+        return False
+
+
+def three_in_coulmn():
+    for place in column_1:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    for place in column_2:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    for place in column_3:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    else:
+        return False
+
+
+def three_diagonal():
+    for place in diagonal_1:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    for place in diagonal_2:
+        if place == X or place == O:
+            return X
+        else:
+            return O
+    else:
+        return False
 
 
 def check_winner():
     ...
 
 def is_gameover():
-    values = places.values()
-    for value in values:
-        if value is not value.isdigit():
-            check_winner()
-    else:
-        ...
+    if all_tiles:
+        check_winner()
 
 
 
