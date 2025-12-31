d55b4473afe77a844afedb37278b398b73afeb5a
gal <newmangal@msn.com>
2025-12-31 19:10:24 +0200

    started testing the logic in main  it doesnt update the data of the  cells therfore it doesnt see the changes or effect win lose condition

--- logic.py
+++ logic.py
@@ -17,8 +17,8 @@ def is_tile_set(CURRENT_PLAYER):
 
 def three_in_row(row_1, row_2, row_3):
     all_rows = row_1, row_2, row_3
-    global three_x
-    global three_o
+    three_x
+    three_o
     for row in all_rows:
         if row == three_x:
             return X
@@ -27,8 +27,8 @@ def three_in_row(row_1, row_2, row_3):
 
 def three_in_coulmn(column_1, column_2, column_3):
     all_columns = column_1, column_2, column_3
-    global three_x
-    global three_o
+    three_x
+    three_o
     for column in all_columns:
         if column == three_x:
             return X
@@ -38,36 +38,42 @@ def three_in_coulmn(column_1, column_2, column_3):
 
 def three_diagonal(diagonal_1, diagonal_2):
     all_diagonals = diagonal_1, diagonal_2
-    global three_x
-    global three_o
+    three_x
+    three_o
     for diagonal in all_diagonals:
         if diagonal == three_x:
             return X
         elif diagonal == three_o:
             return O
 
-def out_of_tiles():
-    if all_tiles:
+def out_of_tiles(all_tiles):
+    for tile in all_tiles:
+        if tile.isdigit():
+            return False
+    return True
+
+
+def check_winner(diagonal_1, diagonal_2, column_1, column_2, column_3, row_1, row_2, row_3, all_tiles):
+    if three_diagonal(diagonal_1, diagonal_2):
+        print(three_diagonal(diagonal_1, diagonal_2))
+        return True
+    elif three_in_coulmn(column_1, column_2, column_3):
+        print(three_in_coulmn(column_1, column_2, column_3))
+        return True
+    elif three_in_row(row_1, row_2, row_3):
+        print(three_in_row(row_1, row_2, row_3))
+        return True
+    elif out_of_tiles(all_tiles):
         print("TIE")
         return True
 
 
-def check_winner():
-    if three_diagonal():
-        print(three_diagonal())
-    if three_in_coulmn():
-        print(three_in_coulmn())
-    if three_in_row():
-        print(three_in_row())
 
 
 def is_gameover():
-    if all_tiles:
-        check_winner()
+    ...
 
 
 
 if __name__ == "__main__":
     ...
-
-    print(three_in_row(row_1, row_2, row_3))
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from board import board_draw
+from board import board_draw, places,  row_1, row_2, row_3, column_2, column_3, column_1, diagonal_1, diagonal_2, all_tiles
 from player_input import  X, O, switch_player
 from logic import is_tile_set
-def turn(CURRENT_PLAYER):
+from logic import check_winner
+
+
+def turn(CURRENT_PLAYER, diagonal_1, diagonal_2, column_1, column_2, column_3, row_1, row_2, row_3, all_tiles):
     while True:
         board_draw()
         is_tile_set(CURRENT_PLAYER)
-        CURRENT_PLAYER = switch_player(CURRENT_PLAYER)
+        if check_winner(diagonal_1, diagonal_2, column_1, column_2, column_3, row_1, row_2, row_3, all_tiles):
+            print("Game Over")
+            return False
         board_draw()
-        is_tile_set(CURRENT_PLAYER)
         CURRENT_PLAYER = switch_player(CURRENT_PLAYER)
+
 
 
 
 if __name__ == "__main__":
     print(f"welcome to a game of Tic Tack Toe {X} , {O}")
     CURRENT_PLAYER = X
-    turn(CURRENT_PLAYER)
+    turn(CURRENT_PLAYER, diagonal_1, diagonal_2, column_1, column_2, column_3, row_1, row_2, row_3, all_tiles)
